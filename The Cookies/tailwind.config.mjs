48e8eeffd8bb5dc7ba6ce7c2cb6e0f1ddf6ee2c3
henriqueolivgp <henriqueoliveira.g.p@gmail.com>
2026-05-25 19:03:07 +0100

    first commit

---
+++ The Cookies/tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  // Isto diz ao Tailwind para procurar classes em todos os teus ficheiros na pasta src
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
